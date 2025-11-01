729d27e5b953ab23e8c81b720e714c7ef65f06f4
Maiquel-Devs <junior.mafra13062005@gmail.com>
2025-11-01 19:56:00 -0300

    ..

---
+++ MeuPrimeiroSpring/MeuPrimeiroSpring/src/main/java/com/example/MeuPrimeiroSpring/MeuPrimeiroSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.MeuPrimeiroSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuPrimeiroSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuPrimeiroSpringApplication.class, args);
+	}
+
+}
